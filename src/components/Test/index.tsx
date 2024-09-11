c80d3f76ea2f694cdec12dfd40556ff9b4f97703
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-09-11 11:33:50 +0900

    test: test컴포넌트 수정

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Draggable from "../Draggable";
-import Droppable, { DropFnProps } from "../Droppable";
+import Droppable from "../Droppable";
 import S from "./style";
 
 interface Item {
@@ -21,79 +21,79 @@ export default function Test() {
   const [itemsInDroppable1, setItemsInDroppable1] = useState<Item[]>(initialItems1);
   const [itemsInDroppable2, setItemsInDroppable2] = useState<Item[]>(initialItems2);
 
-  const handleDrop1 = ({ draggedItemId, targetIndex, draggedIndex, startId }: DropFnProps) => {
-    if (startId === "droppable1") {
-      const draggedItem = itemsInDroppable1.find((item) => String(item.id) === draggedItemId) as Item;
-      setItemsInDroppable1((prev) => {
-        const items = [...prev];
-        if (targetIndex > draggedIndex) {
-          items.splice(draggedIndex, 1);
-          items.splice(targetIndex - 1, 0, draggedItem);
-        }
-        if (targetIndex < draggedIndex) {
-          items.splice(draggedIndex, 1);
-          items.splice(targetIndex, 0, draggedItem);
-        }
-        return items;
-      });
-    } else if (startId === "droppable2") {
-      const draggedItem = itemsInDroppable2.find((item) => String(item.id) === draggedItemId) as Item;
-      setItemsInDroppable2((prev) => {
-        const sourceItems = [...prev];
-        sourceItems.splice(draggedIndex, 1);
-        return sourceItems;
-      });
-      setItemsInDroppable1((prev) => {
-        const newTargetItems = [...prev];
-        newTargetItems.splice(targetIndex, 0, draggedItem);
-        return newTargetItems;
-      });
-    }
-  };
+  // const handleDrop1 = ({ draggedItemId, targetIndex, draggedIndex, startId }: DropFnProps) => {
+  //   if (startId === "droppable1") {
+  //     const draggedItem = itemsInDroppable1.find((item) => String(item.id) === draggedItemId) as Item;
+  //     setItemsInDroppable1((prev) => {
+  //       const items = [...prev];
+  //       if (targetIndex > draggedIndex) {
+  //         items.splice(draggedIndex, 1);
+  //         items.splice(targetIndex - 1, 0, draggedItem);
+  //       }
+  //       if (targetIndex < draggedIndex) {
+  //         items.splice(draggedIndex, 1);
+  //         items.splice(targetIndex, 0, draggedItem);
+  //       }
+  //       return items;
+  //     });
+  //   } else if (startId === "droppable2") {
+  //     const draggedItem = itemsInDroppable2.find((item) => String(item.id) === draggedItemId) as Item;
+  //     setItemsInDroppable2((prev) => {
+  //       const sourceItems = [...prev];
+  //       sourceItems.splice(draggedIndex, 1);
+  //       return sourceItems;
+  //     });
+  //     setItemsInDroppable1((prev) => {
+  //       const newTargetItems = [...prev];
+  //       newTargetItems.splice(targetIndex, 0, draggedItem);
+  //       return newTargetItems;
+  //     });
+  //   }
+  // };
 
-  const handleDrop2 = ({ draggedItemId, targetIndex, draggedIndex, startId }: DropFnProps) => {
-    if (startId === "droppable1") {
-      const draggedItem = itemsInDroppable1.find((item) => String(item.id) === draggedItemId) as Item;
-      setItemsInDroppable1((prev) => {
-        const sourceItems = [...prev];
-        sourceItems.splice(draggedIndex, 1);
-        return sourceItems;
-      });
-      setItemsInDroppable2((prev) => {
-        const newTargetItems = [...prev];
-        newTargetItems.splice(targetIndex, 0, draggedItem);
-        return newTargetItems;
-      });
-    } else if (startId === "droppable2") {
-      const draggedItem = itemsInDroppable2.find((item) => String(item.id) === draggedItemId) as Item;
-      setItemsInDroppable2((prev) => {
-        const items = [...prev];
-        if (targetIndex > draggedIndex) {
-          items.splice(draggedIndex, 1);
-          items.splice(targetIndex - 1, 0, draggedItem);
-        }
-        if (targetIndex < draggedIndex) {
-          items.splice(draggedIndex, 1);
-          items.splice(targetIndex, 0, draggedItem);
-        }
-        return items;
-      });
-    }
-  };
+  // const handleDrop2 = ({ draggedItemId, targetIndex, draggedIndex, startId }: DropFnProps) => {
+  //   if (startId === "droppable1") {
+  //     const draggedItem = itemsInDroppable1.find((item) => String(item.id) === draggedItemId) as Item;
+  //     setItemsInDroppable1((prev) => {
+  //       const sourceItems = [...prev];
+  //       sourceItems.splice(draggedIndex, 1);
+  //       return sourceItems;
+  //     });
+  //     setItemsInDroppable2((prev) => {
+  //       const newTargetItems = [...prev];
+  //       newTargetItems.splice(targetIndex, 0, draggedItem);
+  //       return newTargetItems;
+  //     });
+  //   } else if (startId === "droppable2") {
+  //     const draggedItem = itemsInDroppable2.find((item) => String(item.id) === draggedItemId) as Item;
+  //     setItemsInDroppable2((prev) => {
+  //       const items = [...prev];
+  //       if (targetIndex > draggedIndex) {
+  //         items.splice(draggedIndex, 1);
+  //         items.splice(targetIndex - 1, 0, draggedItem);
+  //       }
+  //       if (targetIndex < draggedIndex) {
+  //         items.splice(draggedIndex, 1);
+  //         items.splice(targetIndex, 0, draggedItem);
+  //       }
+  //       return items;
+  //     });
+  //   }
+  // };
 
   return (
     <div style={{ display: "flex", gap: "20px" }}>
-      <Droppable onDrop={handleDrop1}>
+      <Droppable onDrop={(props) => console.log(props)} droppableId="droppable1">
         {itemsInDroppable1.map((item, index) => (
-          <Draggable droppableId={"droppable1"} key={item.id} id={String(item.id)} index={index}>
+          <Draggable key={item.id} id={String(item.id)} index={index}>
             <S.DragContent1>{item.value}</S.DragContent1>
           </Draggable>
         ))}
       </Droppable>
 
-      <Droppable onDrop={handleDrop2}>
+      <Droppable onDrop={(props) => console.log(props)} droppableId="droppable2">
         {itemsInDroppable2.map((item, index) => (
-          <Draggable droppableId={"droppable2"} key={item.id} id={String(item.id)} index={index}>
+          <Draggable key={item.id} id={String(item.id)} index={index}>
             <S.DragContent2>{item.value}</S.DragContent2>
           </Draggable>
         ))}
