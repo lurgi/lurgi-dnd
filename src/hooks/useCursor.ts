96d5878c668c508452f634e41c090b19bf320ae2
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-09-11 22:32:57 +0900

    feat: useCursor훅 구현

---
+++ src/hooks/useCursor.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+
+type CursorState = {
+  element?: HTMLDivElement;
+  handleMouseMove?: (event: MouseEvent) => void;
+  handleMouseUp?: () => void;
+
+  renderCursor: (Element: HTMLDivElement, handleMouseUp: () => void) => void;
+  deleteCursor: () => void;
+};
+
+export const useCursor = create<CursorState>((set, get) => ({
+  renderCursor: (Element, handleMouseUp) => {
+    Element.id = "custom-cursor";
+    Element.style.position = "fixed";
+    Element.style.pointerEvents = "none";
+    Element.style.zIndex = "9999";
+    document.body.appendChild(Element);
+
+    const handleMouseMove = (e: MouseEvent) => {
+      Element!.style.left = `${e.clientX}px`;
+      Element!.style.top = `${e.clientY}px`;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+
+    set({
+      element: Element,
+      handleMouseMove,
+      handleMouseUp,
+    });
+  },
+
+  deleteCursor: () => {
+    const { element, handleMouseMove, handleMouseUp } = get();
+    element?.remove();
+    if (handleMouseMove) document.removeEventListener("mousemove", handleMouseMove);
+    if (handleMouseUp) document.removeEventListener("mouseup", handleMouseUp);
+  },
+}));
